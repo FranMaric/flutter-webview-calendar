f83c8f6dcf333e70840826f6436583947c198bef
Fran Marić <fran.maric.vk@gmail.com>
2022-04-02 14:42:41 +0200

    Add desktop user agent

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+const desktopUserAgent =
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -11,6 +14,7 @@ class HomeScreen extends StatelessWidget {
         child: WebView(
           initialUrl: 'https://calendar.google.com/calendar/u/0',
           javascriptMode: JavascriptMode.unrestricted,
+          userAgent: desktopUserAgent,
         ),
       ),
     );
